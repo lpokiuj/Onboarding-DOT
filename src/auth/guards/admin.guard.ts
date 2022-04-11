45258f9053768202cd5dd58a1c670789c1c6e86e
lpokiuj <michael082@binus.ac.id>
2022-04-11 22:53:08 +0700

    Authorization done

--- src/auth/guards/admin.guard.ts
+++ src/auth/guards/admin.guard.ts
@@ -1,13 +1,10 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-  constructor(private reflector: Reflector) {
-  }
 
-  canActivate( context: ExecutionContext, ): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate( context: ExecutionContext ): boolean | Promise<boolean> | Observable<boolean> {
 
     const ctx = context.switchToHttp();
     const request = ctx.getRequest(); 
